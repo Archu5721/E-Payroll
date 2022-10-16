3e79677a35bc1bd5e385d55900ba88784e7cc6f3
Archatha <archatha18022@cse.ssn.edu.in>
2022-10-16 22:14:24 +0530

    Register Employee Added

--- Employeecontrols/src/app/register/register.component.ts
+++ Employeecontrols/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  onAddEmployee(employeeData: { emp_name:string,date_of_joining:string,email:string,designation:string,salary:number,gender:string,age:number,password:string }, form: NgForm) {
+    this.http.post("http://localhost:8080/Employees/addemployee", employeeData).subscribe((responseData) => {
+      // console.log(responseData);
+      alert("Employee added Successfully!!");
+      form.reset();
+    });
   }
 
 }
